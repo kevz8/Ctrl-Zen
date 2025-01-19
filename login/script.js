6acbacac50bbcc77636900885c286457c25d73d2
henilagrawal <hagraw02@students.cs.ubc.ca>
2025-01-19 11:44:00 -0800

    finished version

--- login/script.js
+++ login/script.js
@@ -9,7 +9,7 @@ document.getElementById('signInButton').addEventListener('click', async function
         console.log("success");
         // window.open("ui copy 7/index.html", "_blank");
         //window.location.href = "http://localhost:63342/Ctrl-Zen/ui%20copy%207/index.html?_ijt=3me3j23mi8obsektrqlfr8ijji";
-        window.location.href = "file:///Users/henilagrawal/Ctrl-Zen-1/frontend/index.html";
+        window.location.href = "../frontend/index.html";
     } else {
         console.log("fail");
     }
